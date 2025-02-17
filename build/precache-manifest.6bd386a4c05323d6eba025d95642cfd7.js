dd26a37e7e2002556ce0aae64c62b011b7d80610
Sarab Anand Sehgal <SarabAnand.Sehgal@georgebrown.ca>
2025-02-16 20:33:44 -0500

    Updated

---
+++ build/precache-manifest.6bd386a4c05323d6eba025d95642cfd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e0c46ac420081e5c9980129d51ef21",
+    "url": "/comp2156/index.html"
+  },
+  {
+    "revision": "b42a54df77dbcce5427a",
+    "url": "/comp2156/static/css/main.179e7dee.chunk.css"
+  },
+  {
+    "revision": "d4cf2ad043c5ea406caf",
+    "url": "/comp2156/static/js/2.6a101ebd.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/comp2156/static/js/2.6a101ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b26eb387e785008380",
+    "url": "/comp2156/static/js/3.2fa9004c.chunk.js"
+  },
+  {
+    "revision": "b42a54df77dbcce5427a",
+    "url": "/comp2156/static/js/main.e4f489e0.chunk.js"
+  },
+  {
+    "revision": "1b31dc5ae46c33a6fa53",
+    "url": "/comp2156/static/js/runtime-main.3088a121.js"
+  }
+]);
